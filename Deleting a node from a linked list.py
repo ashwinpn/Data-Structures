dffa102546f4c4c7b6077b504cef7f6739b83c06
Ashwin Prakash Nalwade <53330017+ashwinpn@users.noreply.github.com>
2020-01-30 20:13:44 -0500

    Create Deleting a node from a linked list.py

---
+++ Deleting a node from a linked list.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None 
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None 
+        
+    def push(self, new_data):
+        newNode = Node(new_data)
+        newNode.next = self.head 
+        self.head = newNode 
+        
+    def deleteNode(self, key):
+        temp = self.head
+        
+        if temp is not None:
+            if temp.data == key:
+                self.head = temp.next
+                temp = None
+                return 
+            
+        while (temp is not None):
+            if temp.data == key:
+                break 
+            prev = temp 
+            temp = temp.next 
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next 
+            
+        if temp == None:
+            return
+        
+        prev.next = temp.next
+        
+        temp = None 
